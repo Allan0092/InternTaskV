0344741b615b78881a8f3221a86ebf2d1d8cd76c
Allan Gautam <allan.gtm92@gmail.com>
2026-04-15 12:27:13 +0545

    added new lifecycle for orderItem status

--- src/utils/lifecycle.ts
+++ src/utils/lifecycle.ts
@@ -1,35 +1,71 @@
-import { OrderStatus } from "@/generated/prisma/enums.js";
+import { OrderItemStatus, OrderStatus } from "@/generated/prisma/enums.js";
+import { AppError } from "@/types/index.js";
 
-const orderStatusLifeCycle = (curr: OrderStatus, next: OrderStatus) => {
+const orderStatusLifeCycle = (
+  currStatus: OrderStatus,
+  newStatus: OrderStatus,
+) => {
   // PENDING -> PROCESSING -> SHIPPING -> COMPLETED
   // PENDING -> PROCESSING -> DECLINED
   // PENDING -> DECLINED
 
-  if (next === OrderStatus.PENDING) {
+  if (newStatus === OrderStatus.PENDING) {
     return false;
   }
 
-  if (curr === OrderStatus.PENDING) {
-    if (next === OrderStatus.PROCESSING) {
+  if (currStatus === OrderStatus.PENDING) {
+    if (newStatus === OrderStatus.PROCESSING) {
       return true;
-    } else if (next === OrderStatus.DECLINED) {
-      return true;
-    }
-  }
-
-  if (curr === OrderStatus.PROCESSING) {
-    if (next === OrderStatus.DECLINED) {
-      return true;
-    } else if (next === OrderStatus.SHIPPING) {
+    } else if (newStatus === OrderStatus.DECLINED) {
       return true;
     }
   }
 
-  if (curr === OrderStatus.SHIPPING && next === OrderStatus.COMPLETED) {
+  if (currStatus === OrderStatus.PROCESSING) {
+    if (newStatus === OrderStatus.DECLINED) {
+      return true;
+    } else if (newStatus === OrderStatus.SHIPPING) {
+      return true;
+    }
+  }
+
+  if (
+    currStatus === OrderStatus.SHIPPING &&
+    newStatus === OrderStatus.COMPLETED
+  ) {
     return true;
   }
 
   return false;
 };
 
-export { orderStatusLifeCycle };
+const orderItemStatusLifecycle = (
+  currStatus: OrderItemStatus,
+  newStatus: OrderItemStatus,
+) => {
+  // Final Status
+  switch (currStatus) {
+    case OrderItemStatus.SHIPPED:
+      throw new AppError("Cannot change order item status from Shipped", 401);
+    case OrderItemStatus.DECLINE:
+      throw new AppError("Cannot change order item status from Declined", 401);
+    case OrderItemStatus.COMPLETED:
+      throw new AppError("Order has already been Completed", 401);
+  }
+
+  if (currStatus === OrderItemStatus.PENDING) {
+    if (newStatus === OrderItemStatus.PROCESSING) {
+      return true;
+    } else if (newStatus === OrderItemStatus.DECLINE) {
+      return true;
+    }
+  }
+  if (currStatus === OrderItemStatus.PROCESSING) {
+    if (newStatus === OrderItemStatus.SHIPPED) {
+      return true;
+    }
+  }
+  throw new AppError("Could not change the status", 401);
+};
+
+export { orderItemStatusLifecycle, orderStatusLifeCycle };
